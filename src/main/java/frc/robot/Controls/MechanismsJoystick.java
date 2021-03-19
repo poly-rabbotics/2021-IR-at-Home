c7dd5d0d1ef519d3936527ba525511abee9bd6b3
Josef Morales <jmoral2005@gmail.com>
2021-03-19 15:32:14 -0700

    Update MechanismsJoystick.java

--- src/main/java/frc/robot/Controls/MechanismsJoystick.java
+++ src/main/java/frc/robot/Controls/MechanismsJoystick.java
@@ -5,8 +5,13 @@ import frc.robot.RobotMap;
 
 public class MechanismsJoystick {
     private static Joystick joystick = RobotMap.mechanismsJoystick;
+    public static boolean isManual = false;
+public static String controlState = "Normal";
 
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Auto Modes-----------------------------------------------------------------------------------------------------------------------------------
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static boolean startGalacticSearch(){
     return  joystick.getRawButton(4); //needs to be given an actual number
       }
@@ -17,4 +22,44 @@ public class MechanismsJoystick {
       return joystick.getRawButton(5); //port #
     }
 
-}
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   // SHOOTER ADJUST --------------------------------------------------------------------------------------------------------------------------------
+   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static double getChangeTopShooter() {
+      return joystick.getRawAxis(3);
+    }
+    public static double getChangeBottomShooter() {
+      return joystick.getRawAxis(5);
+    }
+    public static boolean allowCalibrateShooter(){
+      return joystick.getRawButton(3);
+    }
+  
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     // SHOOTER CONTROLS  ---------------------------------------------------------------------------------------------------------------------------
+     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     public static boolean getToggleManShootOne(){
+      return joystick.getRawButton(5);
+    }
+    public static boolean getToggleManShootTwo(){
+      return joystick.getRawButton(6);
+    }
+  
+    public static boolean getToggleManShootThree(){
+      return joystick.getRawButton(7);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // MANUAL ONLY COMMANDS ----------------------------------------------------------------------------------------------------------------------
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static boolean isManual(){
+      if(isManual == true){
+        controlState = "Manual";
+      }
+      else{
+        controlState = "Normal";
+      }
+      return joystick.getRawButton(11);
+    }}
+
+//somehow works with no bracket here?
